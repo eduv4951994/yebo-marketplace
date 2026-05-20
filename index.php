5ee1dd9d37ef26de043f41bdd82cab3ee71ed32c
Callen Krige <eduv4951994@vossie.net>
2026-05-20 14:33:32 +0200

    Fixed css linking issue on index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YEBO Marketplace - Home</title>
     
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
 
